d8dba0b389e62f3b94737e06af21462bc2940434
João Pedro <joaopedrov159@hotmail.com>
2024-07-17 10:07:57 -0300

    Entidade User e Resource

---
+++ src/main/java/com/jpob/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.jpob.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jpob.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "984652315", "12345678");
+		return ResponseEntity.ok().body(u);
+	}
+}
