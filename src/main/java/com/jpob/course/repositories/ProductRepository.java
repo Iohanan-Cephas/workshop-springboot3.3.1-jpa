ef258e6696c1ccaef9dd1590b30ac69ac85251e4
João Pedro <joaopedrov159@hotmail.com>
2024-07-27 09:34:37 -0300

    Product

---
+++ src/main/java/com/jpob/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.jpob.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jpob.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+}
